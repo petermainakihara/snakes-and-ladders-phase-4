c6c425a4bc89520b5c58cfb7b83903f1cadc3bba
petermainakihara <pkihara648@gmail.com>
2025-09-25 09:00:00 +0300

    fix main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,6 @@ import './index.css'
 import App from './App.jsx'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GameState from "./GameState"; // If it's directly in src/
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
